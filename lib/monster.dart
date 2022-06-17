8c7491f754e4b2825aeac7cbc9d1fda2b452acfc
your username <rodrigodjr1@gmail.com>
2022-06-17 02:14:39 -0300

    saindo a batalha

---
+++ lib/monster.dart
@@ -0,0 +1,23 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+
+import 'dados.dart';
+
+class Monster {
+  var hp;
+  var dmg;
+  var endu;
+  var imagem;
+
+  Monster(var hp, var dmg, var endu, var image) {
+    this.hp = hp;
+    this.dmg = dmg;
+    this.endu = endu;
+    this.imagem = image;
+  }
+
+  List<Monster> monstroEncouter = [
+    Monster(100, 20, 16, Image.asset('m.png')),
+    Monster(150, 16, 26, Image.asset('monstro.gif'))
+  ];
+}
