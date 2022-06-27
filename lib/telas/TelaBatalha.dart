5692dceecbd2899bdd22cc66a517ff7dcd2e3bee
your username <rodrigodjr1@gmail.com>
2022-06-27 02:49:22 -0300

    continuando

--- lib/telas/TelaBatalha.dart
+++ lib/telas/TelaBatalha.dart
@@ -19,14 +19,26 @@ class Batalha1 extends StatefulWidget {
 class _Batalha1State extends State<Batalha1> {
   HeroBatalha b = HeroBatalha();
   Atributos att = Atributos();
-  double monsterHP = 100;
-  double hP = 100;
-  double mana = 100;
-  var duration = const Duration(milliseconds: 1);
+  num? monsterHP;
+  num? hP;
+  num? mana;
+  String nome = '';
+  var duration = const Duration(seconds: 1);
+
+  @override
+  void initState() {
+    super.initState();
+    att.generateMonster();
+    att.getAtts();
+    monsterHP = att.monsterHp;
+    hP = att.hp;
+    mana = att.mana;
+    nome = att.nome;
+  }
 
   Widget char() {
     return Container(
-        margin: const EdgeInsets.only(top: 220),
+        margin: const EdgeInsets.only(top: 120),
         height: 210,
         width: 170,
         child: AnimatedPadding(
@@ -47,19 +59,19 @@ class _Batalha1State extends State<Batalha1> {
 
   Widget monsterChar() {
     return Container(
-        margin: const EdgeInsets.only(top: 220),
-        height: 210,
-        width: 170,
+        margin: const EdgeInsets.only(top: 100),
+        height: 230,
+        width: 200,
         child: AnimatedPadding(
           duration: const Duration(milliseconds: 500),
           curve: Curves.easeInOutExpo,
           padding: const EdgeInsets.only(left: 0),
           child: Row(
             children: [
-              Image.asset(
-                "lib/images/monstro.gif",
-                width: 150,
-                height: 210,
+              Image.network(
+                att.monsterImagem,
+                width: 200,
+                height: 230,
               )
             ],
           ),
@@ -69,28 +81,77 @@ class _Batalha1State extends State<Batalha1> {
   void ataque() {
     setState(() {
       att.getAtts();
-      monsterHP = att.monsterHp as double;
-      hP = att.hp as double;
-      mana = att.mana as double;
+      monsterHP = att.monsterHp;
+      hP = att.hp;
+      mana = att.mana;
     });
   }
 
-  Future<void> alerta() async {
+  Future<void> alertaWin() async {
     return showDialog<void>(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
             title: const Text('Vitória!'),
-            content: const Text('Seguir para a proxima sala'),
+            content: Text(
+                'Seguir para a proxima sala \n O que deseja recuperar ' +
+                    nome +
+                    '?'),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: TextButton(
+                        onPressed: () {
+                          att.getAtts();
+                          att.hp = 100;
+                          att.setHero();
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Batalha1()));
+                        },
+                        child: const Text('HP')),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: TextButton(
+                        onPressed: () {
+                          att.getAtts();
+                          att.mana = 100;
+                          att.setHero();
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Batalha1()));
+                        },
+                        child: const Text('Mana')),
+                  ),
+                ],
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> alertaLose() async {
+    return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Derrota T_T'),
+            content: const Text('Retornar para o menu'),
             actions: [
               TextButton(
                   onPressed: () {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const TelaVitoria()));
+                            builder: (context) => const Inicial()));
                   },
-                  child: const Text('Proximo')),
+                  child: const Text('OK')),
             ],
           );
         });
@@ -104,7 +165,7 @@ class _Batalha1State extends State<Batalha1> {
       child: Column(children: [
         Container(
           padding:
-              const EdgeInsets.only(right: 30, left: 30, top: 50, bottom: 10),
+              const EdgeInsets.only(right: 10, left: 10, top: 50, bottom: 10),
           height: 516,
           width: double.infinity,
           decoration: const BoxDecoration(
@@ -113,12 +174,20 @@ class _Batalha1State extends State<Batalha1> {
                   fit: BoxFit.cover)),
           child: Column(
             children: [
-              LinearProgressIndicator(
-                backgroundColor: Colors.white,
-                valueColor: const AlwaysStoppedAnimation(Colors.red),
-                minHeight: 18,
-                value: monsterHP,
-                semanticsValue: monsterHP.toString(),
+              Container(
+                //HP do Monstro
+                color: Colors.red,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Text('HP: $monsterHP')]),
+              ),
+              Container(
+                height: 100,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -139,27 +208,25 @@ class _Batalha1State extends State<Batalha1> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
-                width: 350,
-                margin: const EdgeInsets.only(top: 10),
-                child: LinearProgressIndicator(
-                  backgroundColor: Colors.white,
-                  valueColor: const AlwaysStoppedAnimation(Colors.green),
-                  minHeight: 18,
-                  value: hP,
-                  semanticsValue: hP.toString(),
-                ),
-              ),
+                  width: 350,
+                  margin: const EdgeInsets.only(top: 10),
+                  child: Container(
+                    color: Colors.green,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text('HP: $hP')]),
+                  )),
               Container(
-                width: 350,
-                margin: const EdgeInsets.all(10),
-                child: LinearProgressIndicator(
-                  backgroundColor: Colors.white,
-                  valueColor: const AlwaysStoppedAnimation(Colors.blue),
-                  minHeight: 18,
-                  value: mana,
-                  semanticsValue: mana.toString(),
-                ),
-              ),
+                  width: 350,
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.white,
+                  height: 18,
+                  child: Container(
+                    color: Colors.blue,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text('Mana: $mana')]),
+                  )),
               Container(
                 width: double.infinity,
                 margin: const EdgeInsets.only(
@@ -174,19 +241,13 @@ class _Batalha1State extends State<Batalha1> {
                     ),
                     onPressed: () {
                       b.batalhar(1);
-                      att.getAtts();
                       ataque();
                       if (att.monsterHp <= 0) {
-                        alerta();
+                        alertaWin();
                       } else {
-                        sleep(duration);
                         b.monsterATK();
                         if (att.hp <= 0) {
-                          sleep(duration);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Inicial()));
+                          alertaLose();
                         }
                       }
                     },
@@ -207,19 +268,13 @@ class _Batalha1State extends State<Batalha1> {
                     child: const Text('Skill 2'),
                     onPressed: () {
                       b.batalhar(2);
-                      att.getAtts();
                       ataque();
                       if (att.monsterHp <= 0) {
-                        alerta();
+                        alertaWin();
                       } else {
-                        sleep(duration);
                         b.monsterATK();
                         if (att.hp <= 0) {
-                          sleep(duration);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Inicial()));
+                          alertaLose();
                         }
                       }
                     },
@@ -239,19 +294,13 @@ class _Batalha1State extends State<Batalha1> {
                     child: const Text('Skill 3'),
                     onPressed: () {
                       b.batalhar(3);
-                      att.getAtts();
                       ataque();
                       if (att.monsterHp <= 0) {
-                        alerta();
+                        alertaWin();
                       } else {
-                        sleep(duration);
                         b.monsterATK();
                         if (att.hp <= 0) {
-                          sleep(duration);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Inicial()));
+                          alertaLose();
                         }
                       }
                     },
